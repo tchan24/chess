1f8c0596db93336fa09bd322f6b35b947a760d54
tchan24 <tchan0524@gmail.com>
2024-03-30 18:35:54 -0500

    setup main and constants

---
+++ src/const.py
@@ -0,0 +1,8 @@
+# screen dimension
+WIDTH = 1000
+HEIGHT = 1000
+
+#board dimension
+ROWS = 8
+COLUMNS = 8
+SQSIZE = WIDTH // COLUMNS
---
+++ src/main.py
@@ -0,0 +1,9 @@
+class Main:
+    def __init__(self):
+        pass
+
+    def mainloop(self):
+        pass
+
+main = Main()
+main.mainloop()
